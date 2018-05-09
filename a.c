f305e522011b6fb7507c678a762d54ff259092d4
mastodilu <matteo.dilucchio@edu.unito.it>
2018-05-09 16:57:51 +0200

    a.c creates private message queue of love

--- a.c
+++ a.c
@@ -41,13 +41,25 @@ int main(int argc, char* argv[])
         myself.type, myself.name, myself.genome, sem_init_people, sem_init_people2, msgq );
 
 
+    //create personal message queue of love
+	love_msg_queue = msgget(IPC_PRIVATE, 0666|IPC_CREAT|IPC_EXCL);
+	if( love_msg_queue == -1 ){
+		if( errno == EEXIST ){
+			if( msgctl(love_msg_queue, IPC_RMID, &msq) == -1 )
+				errExit("rmid queue of love");
+		}else
+			errExit("msgget queue of love");
+	}
+    printf("A <3queue<3:%d\n", love_msg_queue);
+
+
     //create message to introduce A to others
     welcome.mtype = (long)myself.genome;
     welcome.mtxt.pid = (int)getpid();
     welcome.mtxt.type = 'A';
     welcome.mtxt.name = myself.name;
     welcome.mtxt.genome = myself.genome;
-    welcome.mtxt.key_of_love = -1;
+    welcome.mtxt.key_of_love = love_msg_queue;
     welcome.mtxt.partner = -1;
 
     if( msgsnd(msgq, &welcome, sizeof(welcome), 0) == -1 ){
